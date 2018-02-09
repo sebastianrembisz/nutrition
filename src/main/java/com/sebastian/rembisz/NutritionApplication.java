2fbacd17e6a06f13650e0f6863985f99c0f7c18b
Sebastian Rembisz <sebastian.rembisz@gmail.com>
2018-02-09 21:05:37 +0100

    initial commit

---
+++ src/main/java/com/sebastian/rembisz/NutritionApplication.java
@@ -0,0 +1,12 @@
+package com.sebastian.rembisz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutritionApplication.class, args);
+	}
+}
